93e87cc514f581fe8108453f400d8d9a2b7a32eb
jchen20 <43321176+jchen20@users.noreply.github.com>
2020-04-27 20:35:17 -0400

    generator

--- code/generator.py
+++ code/generator.py
@@ -1,18 +1,11 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Apr 23 19:40:50 2020
-
-@author: Jeremy
-"""
-
 import numpy as np
 import tensorflow as tf
+from spadeblock import SpadeBlock
 
-def upsample(batch_inputs):
-    return tf.image.resize(batch_inputs, [2*np.shape(batch_inputs)[1], 2*np.shape(batch_inputs)[2]])
+
 
 class SPADEGenerator(tf.keras.Model):
-    def __init__(self, opt):
+    def __init__(self):
         super(SPADEGenerator, self).__init__()
         
         self.beta1 = 0.0
@@ -34,11 +27,11 @@ class SPADEGenerator(tf.keras.Model):
         self.spade_layers.append(SpadeBlock(256, 128))
         self.spade_layers.append(SpadeBlock(128, 64))
         self.conv_layer = tf.keras.layers.Conv2D(64, (3,3), activation='tanh')
-    def call(images):
+    def call(self, images):
         batch_size = np.shape(images)[0]
         noise = np.random.normal((batch_size, 1024))
         result_dense = self.dense(noise)
-        reshaped = tf.reshape(result, [batch_size, self.image_width, self.image_height, self.num_channels])
+        reshaped = tf.reshape(result_dense, [batch_size, self.image_width, self.image_height, self.num_channels])
         result = self.spade_layers[0](reshaped, images)
         for layer in self.spade_layers[1:]:
             result = upsample(result)
@@ -46,3 +39,6 @@ class SPADEGenerator(tf.keras.Model):
         result = tf.nn.leaky_relu(result)
         result = self.conv_layer(result)
         return result
+    
+def upsample(batch_inputs):
+    return tf.image.resize(batch_inputs, [2*np.shape(batch_inputs)[1], 2*np.shape(batch_inputs)[2]])
