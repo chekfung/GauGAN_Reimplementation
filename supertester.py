7fd573ecfb221eae860797ae7bd85d01e2df38ec
cheungs01 <44142677+cheungs01@users.noreply.github.com>
2020-04-29 17:35:53 -0400

    Update supertester.py

--- supertester.py
+++ supertester.py
@@ -5,15 +5,15 @@ from test_layer import MyDenseLayer
 import tensorflow as tf
 
 class MOO(Model):
-    def __init__(self):
-        super(MOO, self).__init__()
-        self.layer = MyDenseLayer(10)
-        self.optimizer = tf.keras.optimizers.Adam(learning_rate = 0.0001, beta_1 = 0.5, beta_2 = 0.999)
+	def __init__(self):
+		super(MOO, self).__init__()
+		self.layer = MyDenseLayer(10)
+		self.optimizer = tf.keras.optimizers.Adam(learning_rate = 0.0001, beta_1 = 0.5, beta_2 = 0.999)
 
 
-    def call(self, input):
-        return self.layer(input)
-        
+	def call(self, input):
+		return self.layer(input)
+		
 
 random = tf.random.normal((4, 4))
 
@@ -27,11 +27,12 @@ moo_obj = MyDenseLayer(10)
 
 # Do stuff
 with tf.GradientTape() as tape: 
-    output = moo_obj(random)
-    #output = onion_layer(random, random)
-    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(tf.ones([3,1]), tf.zeros([3,1])))
+	output = moo_obj(random)
+	#output = onion_layer(random, random)
+	loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(tf.ones([3,1]), tf.zeros([3,1])))
 
 	grads = tape.gradient(loss, moo_obj.trainable_variables)
+	print(grads)
 moo_obj.optimizer.apply_gradients(zip(grads, moo_obj.trainable_variables))
 
 print(moo_obj.trainable_variables)
