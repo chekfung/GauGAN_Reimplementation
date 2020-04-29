518bcd0d01705920b80a1eaf8a57c239158c8d43
cheungs01 <44142677+cheungs01@users.noreply.github.com>
2020-04-29 16:42:52 -0400

    Update supertester.py

--- supertester.py
+++ supertester.py
@@ -1,6 +1,13 @@
 from code.spadelayer import SpadeLayer  
-import 
+#import 
+
+random = tf.random.normal((1,1))
 
 layers_of_onion = 10
 onion_layer = SpadeLayer(layers_of_onion)
+
+# Do stuff
+onion_layer(random)
+
+print(onion_layer.losses)
 
