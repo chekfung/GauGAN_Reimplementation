1daa8d57cdfcf0d7c1091097978a7992e8425355
cheungs01 <44142677+cheungs01@users.noreply.github.com>
2020-04-29 16:56:05 -0400

    Update supertester.py

--- supertester.py
+++ supertester.py
@@ -10,7 +10,7 @@ onion_layer = SpadeLayer(layers_of_onion)
 # Do stuff
 with tf.GradientTape() as tape: 
 	output = onion_layer(random, random)
-	loss = 5
+	loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(tf.ones([3,1]), tf.zeros([3,1])))
 
 grads = tape.gradient(loss, onion_layer.trainable_variables)
 
