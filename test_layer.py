ff0e2bf93e004499a0998ab5880853e3970f4263
cheungs01 <44142677+cheungs01@users.noreply.github.com>
2020-04-29 17:24:19 -0400

    Update test_layer.py

--- test_layer.py
+++ test_layer.py
@@ -4,15 +4,15 @@ from tensorflow.keras.layers import Conv2D, BatchNormalization, ReLU, Layer, Den
 
 class MyDenseLayer(tf.keras.Model):
   def __init__(self, num_outputs):
-    super(MyDenseLayer, self).__init__()
-    self.num_outputs = num_outputs
-    self.fc = tf.keras.layers.Dense(num_outputs)
+	super(MyDenseLayer, self).__init__()
+	self.num_outputs = num_outputs
+	self.fc = tf.keras.layers.Dense(num_outputs)
 	self.optimizer = self.optimizer = tf.keras.optimizers.Adam(learning_rate = 0.0001, beta_1 = 0.5, beta_2 = 0.999)
 
   def call(self, input):
-    return self.fc(input)
+	return self.fc(input)
 
   def compute_output_shape(self, input_shape):
-    shape = tf.TensorShape(input_shape).as_list()
-    shape[-1] = self.num_outputs
-    return tf.TensorShape(shape)
+	shape = tf.TensorShape(input_shape).as_list()
+	shape[-1] = self.num_outputs
+	return tf.TensorShape(shape)
