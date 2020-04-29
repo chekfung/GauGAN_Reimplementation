084725c89d92f6c38f22d0e6de2eac36e1b48b0f
cheungs01 <44142677+cheungs01@users.noreply.github.com>
2020-04-29 17:01:03 -0400

    Update supertester.py

--- supertester.py
+++ supertester.py
@@ -1,15 +1,27 @@
 from code.spadelayer import SpadeLayer  
+from tensorflow.keras import Model
 import tensorflow as tf
 #import 
+
+class MOO(Model):
+    def __init__(self):
+        super(MOO, self).__init__()
+        self.layer = SpadeLayer(10)
+
+    def call(self, img, segmap):
+        return self.layer(img, segmap)
+        
 
 random = tf.random.normal((4, 4, 4, 4))
 
 layers_of_onion = 10
 onion_layer = SpadeLayer(layers_of_onion)
+moo_obj = MOO()
 
 # Do stuff
 with tf.GradientTape() as tape: 
-	output = onion_layer(random, random)
+    output = moo_obj(random,random)
+	#output = onion_layer(random, random)
 	loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(tf.ones([3,1]), tf.zeros([3,1])))
 
 grads = tape.gradient(loss, onion_layer.trainable_variables)
