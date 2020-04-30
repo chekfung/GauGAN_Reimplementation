0701134c95575484bbe13749271079ad984b12ec
Jason Ho <jason_ho@brown.edu>
2020-04-30 16:39:42 -0400

    THIS IS THE ONE THAT MAYBE WORKS
    USE THIS AS THE MAIN BRANCH AND CREATE OWN BRANCHES FOR TESTING DIFFERENT IMPLEMENTATIONS

---
+++ plotter.py
@@ -0,0 +1,9 @@
+from matplotlib import pyplot as pyplot
+import pandas as pd
+import seaborn as sns
+
+def plot(dir): 
+
+if __name__ == '__main__':
+	data_dir = 'logs/fid_losses_train.csv' if len(sys.argv) == 1 else sys.argv[1]
+	main(data_dir) 
